6ac69c00fd5b85c075faa316965e8a6fe3ca0c60
Shinchan-git <gyoshu.bowie@gmail.com>
2022-03-08 17:40:29 +0900

    Initial commit

---
+++ components/templates/IndexPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { css } from '@emotion/react'
+
+const IndexPage: React.FC = () => {
+  return (
+    <div css={layoutStyle}>
+      Hello!
+    </div>
+  )
+}
+
+const layoutStyle = css`
+  min-height: 100vh;
+`
+
+export default IndexPage
